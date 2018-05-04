1c87197e860ff6d5969e6c2448a239b0b4f77715
Kylep342 <kylep342@gmail.com>
2018-05-04 17:29:41 -0500

    a less clunky implementation (prettier)

--- May_4_2018_Express.py
+++ May_4_2018_Express.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-def letters_numbers():
+
+def assemble_number_from_digits(digits_list):
+    number = 0
+    for pos in range(len(digits_list)):
+        number += digits_list[pos] * 10 ** (len(digits_list) - (pos + 1))
+    return number
+
+
+def express():
     for a in range(10):
         for b in range(10):
             for c in range(10):
                 for d in range(10):
                     for e in range(10):
-                        if a != b != c != d != e and (int(str(a) + str(b) + str(c) + str(c) + str(d) + str(e)) * 4 == int(str(e) + str(d) + str(c) + str(c) + str(b) + str(a))):
-                                print('a: {0}, b: {1}, c: {2}, d: {3}, e: {4}'.format(a, b, c, d, e))
+                        if a != b != c != d != e and assemble_number_from_digits([a, b, c, c, d, e]) * 4 == assemble_number_from_digits([e, d, c, c, b, a]):    
+                            print('a: {0}, b: {1}, c: {2}, d: {3}, e: {4}'.format(a, b, c, d, e))
 
 
 if __name__ == '__main__':
-    letters_numbers()
+    express()
